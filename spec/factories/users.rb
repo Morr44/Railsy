4cc248197c9f1cae4693c69da2d7dabf82696b2a
Jakub <KZLukos@gmail.com>
2015-04-14 13:13:22 +0000

    tests tests tests...

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  Factory :user do
+  
+    sequence(:first_name) { |n| "John{n}"}
+    sequence(:last_name) { |n| "Smith{n}"}    
+    sequence(:email) { |n| "js#{n}gaa@.com"}    
+    password 'password'
+    
+  end
+end

4054e9fc60eb4b54d9f6f8a11cd1b15bdc5bef58
Jakub <KZLukos@gmail.com>
2015-04-21 13:24:57 +0000

    new article test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 feature 'articles' do
   describe 'user visit articles page', type: :feature do
+    
     before :each do
       @article = create(:article)
       @second_article = create(:article)
@@ -14,6 +15,19 @@ feature 'articles' do
       expect(page).to have_content(@second_article.title)
       expect(page).to have_content('footer')
     end
+    
+
+    it 'user create a new article' do
+      visit articles_path
+
+      click_link('New article')
+
+      expect(page).to have_content('You are not authorized to access this page.')
+      expect(page).not_to have_content(@article.title)
+      expect(page.current_url).to eq 'http://www.exqample.com/'
+      
+    end
+    
     
   end
 end
